398a604651341f2ae1466daf40a744bba94af690
Kay <quacoo.boat@gmail.com>
2025-06-14 11:19:41 +0000

    ✨Add initial project setup (#17)
    * ➕ Add tailwindcss and @nuxt/ui
    
    * 🔨 setup eslint
    
    * 🔨 add pre-commit linting
    
    * ✨ Add github linting workflow
    
    * 💚 Fix multiple versions of pnpm specified:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+    '@nuxt/ui',
+  ],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
