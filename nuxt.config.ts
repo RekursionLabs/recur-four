be53b149159e327034d6d0c8202e18ef84d1b14c
quacoo <quacoo.boat@gmail.com>
2025-06-14 10:13:23 +0000

    🔨 setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,25 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/ui",
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
   ],
-});
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+})
