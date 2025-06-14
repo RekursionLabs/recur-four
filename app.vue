098f5a8800ce325080155157e6a7466e5a4d3bd5
quacoo <quacoo.boat@gmail.com>
2025-06-14 05:03:21 +0000

    ➕ Add tailwindcss and @nuxt/ui

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div class="h-screen flex items-center justify-center">
+      <h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        Project Start🎉
+      </h2>
+    </div>
+  </UApp>
 </template>
